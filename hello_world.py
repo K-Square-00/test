e57f12e667a097f190f711fb08e3613804dc75a1
Kankan Zhou <kan.kan.zhou@outlook.com>
2022-05-17 12:26:30 +0000

    kkk

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 import torch 
 
-print("Hello World!")
-print("Hello World!")
-print("Hello World!")
+print(torch.__version__)
+
+device = "cuda" if (torch.cuda.is_available()) else "cpu"
+print(device)
+print(torch.cuda.get_device_name(0))
+
+
+
